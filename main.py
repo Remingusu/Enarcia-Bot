b5e1cf21e8a32b513343a68fc4f70e9c30648382
Remingusu <leween.m33@gmail.com>
2022-04-30 11:13:14 +0200

    Slash commands load , unload , reload

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import discord
 from discord.ext import commands
+from discord_slash import SlashCommand
+from discord_slash.utils.manage_commands import create_option, create_choice
+from discord_slash.utils.manage_components import *
 
 bot = commands.Bot(command_prefix="ù", description="Enarcia bot")
+slash = SlashCommand(bot, sync_commands=True)
 
 bot.remove_command('help')
 
@@ -30,48 +34,70 @@ def owner(ctx):
     return ctx.author.id == 693761548048531509
 
 
-@bot.command()
 @commands.check(owner)
-async def loadAll(ctx):
-    bot.load_extension("admin")
-    bot.load_extension("fun")
-    bot.load_extension("help")
-    bot.load_extension("info")
-    await ctx.send("All extensions have been loaded")
+@slash.slash(name="Load",
+             description="Load an specific extension or all",
+             options=[create_option(name="extension", description="Select the extension", option_type=3, required=True,
+                                    choices=[
+                                          create_choice(name="Admin", value="admin"),
+                                          create_choice(name="Fun", value="fun"),
+                                          create_choice(name="Help", value="help"),
+                                          create_choice(name="Info", value="info"),
+                                          create_choice(name="All", value="all")])])
+async def load(ctx, extension):
+    if extension == "all":
+        bot.load_extension("admin")
+        bot.load_extension("fun")
+        bot.load_extension("help")
+        bot.load_extension("info")
+        await ctx.send("All extensions have been loaded")
+    else:
+        bot.load_extension(extension)
+        await ctx.send("The " + "**" + extension + "**" + " file has been loaded !")
 
 
-@bot.command()
 @commands.check(owner)
-async def load(ctx, name = None):
-    if not name:
-        ctx.send("Please enter a name !")
-    if name:
-        bot.load_extension(name)
-        await ctx.send("The " + "**" + name + "**" + " file has been loaded !")
+@slash.slash(name="Unload",
+             description="Unload an specific extension or all",
+             options=[create_option(name="extension", description="Select the extension", option_type=3, required=True,
+                                    choices=[
+                                          create_choice(name="Admin", value="admin"),
+                                          create_choice(name="Fun", value="fun"),
+                                          create_choice(name="Help", value="help"),
+                                          create_choice(name="Info", value="info"),
+                                          create_choice(name="All", value="all")])])
+async def unload(ctx, extension):
+    if extension == "all":
+        bot.unload_extension("admin")
+        bot.unload_extension("fun")
+        bot.unload_extension("help")
+        bot.unload_extension("info")
+        await ctx.send("All extensions have been unloaded")
+    else:
+        bot.unload_extension(extension)
+        await ctx.send("The " + "**" + extension + "**" + " file has been unloaded !")
 
 
-@bot.command()
 @commands.check(owner)
-async def unload(ctx, name = None):
-    if not name:
-        ctx.send("Please enter a name !")
-    if name:
-        bot.unload_extension(name)
-        await ctx.send("The " + "**" + name + "**" + " file has been unloaded !")
-
-
-@bot.command()
-@commands.check(owner)
-async def reload(ctx, name = None):
-    if not name:
-        ctx.send("Please enter a name !")
-    if name:
-        try:
-            bot.reload_extension(name)
-            await ctx.send("The " + "**" + name + "**" + " file has been reloaded !")
-        except:
-            bot.load_extension(name)
-            await ctx.send("The " + "**" + name + "**" + " file was not loaded but I load it !")
+@slash.slash(name="Reload",
+             description="Reload an specific extension or all",
+             options=[create_option(name="extension", description="Select the extension", option_type=3, required=True,
+                                    choices=[
+                                          create_choice(name="Admin", value="admin"),
+                                          create_choice(name="Fun", value="fun"),
+                                          create_choice(name="Help", value="help"),
+                                          create_choice(name="Info", value="info"),
+                                          create_choice(name="All", value="all")])])
+async def reload(ctx, extension):
+    if extension == "all":
+        bot.reload_extension("admin")
+        bot.reload_extension("fun")
+        bot.reload_extension("help")
+        bot.reload_extension("info")
+        await ctx.send("All extensions have been reloaded")
+    else:
+        bot.load_extension(extension)
+        await ctx.send("The " + "**" + extension + "**" + " file has been reloaded !")
 
 
 bot.run("OTA4OTc4NDc2NjE3NDY1ODc2.YY9mLA.Uuj1sCJ3s0JcRfvNn0lg0mHaLLE")
